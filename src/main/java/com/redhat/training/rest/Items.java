2f3b1f9f3340095914156f3c0dc4770496264a4e
malacourse <malacourse>
2017-06-09 11:10:11 -0400

    text

---
+++ src/main/java/com/redhat/training/rest/Items.java
@@ -0,0 +1,34 @@
+package com.redhat.training.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+//Sets the path to base URL + /hello
+@Path("/items")
+public class Items {
+
+	  // This method is called if TEXT_PLAIN is request
+	  @GET
+	  @Produces(MediaType.TEXT_PLAIN)
+	  public String sayPlainTextItems() {
+	    return "Hello Jersey";
+	  }
+
+	  // This method is called if XML is request
+	  @GET
+	  @Produces(MediaType.TEXT_XML)
+	  public String sayXMLItems() {
+	    return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+	  }
+
+	  // This method is called if HTML is request
+	  @GET
+	  @Produces(MediaType.TEXT_HTML)
+	  public String sayHtmlItems() {
+	    return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+	        + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+	  }}
+	
