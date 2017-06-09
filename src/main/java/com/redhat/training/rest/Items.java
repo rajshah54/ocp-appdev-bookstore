0b9f6e252c3ba01cb7096c9163115acccf63e6bc
malacourse <malacourse>
2017-06-09 11:48:19 -0400

    add items

--- src/main/java/com/redhat/training/rest/Items.java
+++ src/main/java/com/redhat/training/rest/Items.java
@@ -1,16 +1,26 @@
 package com.redhat.training.rest;
 
+import java.util.List;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.redhat.training.service.CatalogService;
+
 
 
 //Sets the path to base URL + /hello
 @Path("/items")
 public class Items {
 
-	  // This method is called if TEXT_PLAIN is request
+	@Inject
+	private CatalogService service;
+
+	
+	// This method is called if TEXT_PLAIN is request
 	  @GET
 	  @Produces(MediaType.TEXT_PLAIN)
 	  public String sayPlainTextItems() {
@@ -28,7 +38,25 @@ public class Items {
 	  @GET
 	  @Produces(MediaType.TEXT_HTML)
 	  public String sayHtmlItems() {
-	    return "<html> " + "<title>" + "Hello Jersey" + "</title>"
-	        + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
-	  }}
+	    //return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+	    //    + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+	    return getItems();
+	  }
+	  
+	  private String getItems()
+	  {
+		  StringBuilder ret = new StringBuilder();
+			List<String> categories = service.getCategories();
+			if (categories != null)
+			{
+				ret.append("size:" + categories.size() + ":");
+				for (int cat=0;cat<categories.size();cat++)
+				{
+					ret.append(categories.get(cat) + ";");
+				}
+			}
+		  return ret.toString();
+	  }
+	  
+}
 	
